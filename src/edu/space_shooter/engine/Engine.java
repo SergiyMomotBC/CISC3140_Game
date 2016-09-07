ce8f9bf3d381cc49448a1093269dedfed7a5c188
SergiyMomotBC <sergiy.momot@outlook.com>
2016-09-07 16:22:20 -0400

    Small update.

--- src/edu/space_shooter/engine/Engine.java
+++ src/edu/space_shooter/engine/Engine.java
@@ -9,6 +9,7 @@ public class Engine
     {
         inputHandler = new InputHandler(window);
         renderer = new Renderer(window, new Dimension(1920, 1080));
+        resourceManager = new ResourceManager(assetsRootFolder);
     }
 
     public static InputHandler getInputHandler()
@@ -16,7 +17,9 @@ public class Engine
         return inputHandler;
     }
     public static Renderer getRenderer() { return renderer; }
+    public static ResourceManager getResourceManager() { return resourceManager; }
 
-    private static InputHandler inputHandler;
-    private static Renderer     renderer;
+    private static InputHandler         inputHandler;
+    private static Renderer             renderer;
+    private static ResourceManager      resourceManager;
 }
