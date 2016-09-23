83f36d092407e024583518ed9313e52563c6f5ac
SergiyMomotBC <sergiy.momot@outlook.com>
2016-09-22 23:04:33 -0400

    Update

--- src/edu/ci/ecs/systems/HUDSystem.java
+++ src/edu/ci/ecs/systems/HUDSystem.java
@@ -3,6 +3,7 @@ package edu.ci.ecs.systems;
 import edu.ci.ecs.GameObject;
 import edu.ci.ecs.GameObjectType;
 import edu.ci.ecs.components.HealthComponent;
+import edu.ci.ecs.components.ScoreComponent;
 import edu.ci.engine.Engine;
 import java.awt.*;
 
@@ -16,29 +17,40 @@ public class HUDSystem implements System
 
         HealthComponent hc = object.getComponent(HealthComponent.class);
 
-        if(hc != null) {
-            Image life = Engine.getResourceManager().loadImage("player_life_ui.png");
-            Image x = Engine.getResourceManager().loadImage("x_ui.png");
-            String name = "numeral" + String.valueOf(hc.getHp()) + ".png";
-            Image number = Engine.getResourceManager().loadImage(name);
+        if(hc != null)
+            renderLivesLeft(hc);
 
-            Engine.getRenderer().renderSprite(
-                    life,
-                    new Point(30, 20),
-                    new Dimension(life.getWidth(null), life.getHeight(null))
-            );
+        ScoreComponent sc = object.getComponent(ScoreComponent.class);
 
-            Engine.getRenderer().renderSprite(
-                    x,
-                    new Point(75, 24),
-                    new Dimension(x.getWidth(null), x.getHeight(null))
-            );
+        if(sc != null)
+        {
 
-            Engine.getRenderer().renderSprite(
-                    number,
-                    new Point(105, 23),
-                    new Dimension(number.getWidth(null), number.getHeight(null))
-            );
         }
+    }
+
+    private void renderLivesLeft(HealthComponent hc)
+    {
+        Image life = Engine.getResourceManager().loadImage("player_life_ui.png");
+        Image x = Engine.getResourceManager().loadImage("x_ui.png");
+        String name = "numeral" + String.valueOf(hc.getHp()) + ".png";
+        Image number = Engine.getResourceManager().loadImage(name);
+
+        Engine.getRenderer().renderSprite(
+                life,
+                new Point(30, 20),
+                new Dimension(life.getWidth(null), life.getHeight(null))
+        );
+
+        Engine.getRenderer().renderSprite(
+                x,
+                new Point(75, 24),
+                new Dimension(x.getWidth(null), x.getHeight(null))
+        );
+
+        Engine.getRenderer().renderSprite(
+                number,
+                new Point(105, 23),
+                new Dimension(number.getWidth(null), number.getHeight(null))
+        );
     }
 }
