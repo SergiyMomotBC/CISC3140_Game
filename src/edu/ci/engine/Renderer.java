83a779edf2cae82de9ee09e0d5117f657533ae57
SergiyMomotBC <sergiy.momot@outlook.com>
2016-10-21 21:52:41 -0400

    Fixed resolution don't scale

--- src/edu/ci/engine/Renderer.java
+++ src/edu/ci/engine/Renderer.java
@@ -1,5 +1,7 @@
 package edu.ci.engine;
 
+import com.sun.org.apache.xml.internal.security.utils.JavaUtils;
+
 import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
@@ -16,6 +18,7 @@ public final class Renderer
         windowTarget.add(frame);
         frame.createBufferStrategy(2);
         buffer = frame.getBufferStrategy();
+        displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
         windowTarget.pack();
     }
 
@@ -26,8 +29,9 @@ public final class Renderer
 
     public void prepareFrame()
     {
-        Graphics g = buffer.getDrawGraphics();
+        Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
         g.setColor(Color.black);
+        g.scale(displayMode.getWidth() / 1920.0, displayMode.getHeight() / 1080.0);
         g.clearRect(0, 0, resolution.width, resolution.height);
         g.fillRect(0, 0, resolution.width, resolution.height);
         g.dispose();
@@ -36,6 +40,7 @@ public final class Renderer
     public void renderTintedBackground()
     {
         Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
+        g.scale(displayMode.getWidth() / 1920.0, displayMode.getHeight() / 1080.0);
         g.setColor(new Color(0.0f, 0.0f, 0.0f, 0.7f));
         g.fillRect(0, 0, 1920, 1080);
         g.dispose();
@@ -44,6 +49,7 @@ public final class Renderer
     public void renderSprite(Image sprite, Point position, Dimension size)
     {
         Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
+        g.scale(displayMode.getWidth() / 1920.0, displayMode.getHeight() / 1080.0);
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.drawImage(sprite, position.x, position.y, size.width, size.height, null);
         g.dispose();
@@ -57,6 +63,7 @@ public final class Renderer
     public void renderText(String text, Point position, int fontSize, Color color)
     {
         Graphics2D g = (Graphics2D) buffer.getDrawGraphics();
+        g.scale(displayMode.getWidth() / 1920.0, displayMode.getHeight() / 1080.0);
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g.setFont(new Font(null, Font.PLAIN, fontSize));
         g.setColor(color);
@@ -69,6 +76,7 @@ public final class Renderer
         buffer.show();
     }
 
+    private DisplayMode         displayMode;
     private Canvas              frame;
     private BufferStrategy      buffer;
     private Dimension           resolution;
